9fa7a030ef48777d0881c6fd4ccf0373b605af27
pinnintipavan9989 <pinnintipavan9989@gmail.com>
2026-02-15 05:46:59 +0530

    Initial commit add code 32_Longest Valid Parentheses

---
+++ easy/Array/32_Longest_Valid_Parentheses.py
@@ -0,0 +1,19 @@
+def longestValidParentheses(s):
+    stack = [-1]
+    max_len = 0
+
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(i)
+        elif s[i] == ')':
+            stack.pop()
+            if stack == []:
+                stack.append(i)
+            else:
+                lent = i - stack[-1]
+                max_len = max(max_len, lent)
+    return max_len
+
+print(longestValidParentheses("(()")) 
+
+print(longestValidParentheses(")()())")) 
