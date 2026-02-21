00e6baf0196a4f3cff18ba3ed76c0d0807a04f73
pinnintipavan9989 <pinnintipavan9989@gmail.com>
2026-02-21 06:07:23 +0530

    reslove: problem 468 valid IP with example

--- easy/String/468_validate_ip_address.py
+++ easy/String/468_validate_ip_address.py
@@ -22,3 +22,14 @@ def validIPAddress(queryIP):
     
     return "Neither"
 
+examples = [
+    "172.16.254.1",                       # IPv4
+    "256.256.256.256",                    # Neither ( >255 )
+    "192.168.01.1",                       # Neither (leading zero)
+    "2001:0db8:85a3:0000:0000:8a2e:0370:7334",  # IPv6
+    "2001:db8:85a3::8A2E:037j:7334",       # Neither (invalid char j)
+    "1e1.4.5.6"                           # Neither
+]
+
+for ip in examples:
+    print(ip, "->", validIPAddress(ip))
