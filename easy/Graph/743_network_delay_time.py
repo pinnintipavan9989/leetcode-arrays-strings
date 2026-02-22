3fcbd4e6fed2b4015642c7fc3a831580c93c1131
pinnintipavan9989 <pinnintipavan9989@gmail.com>
2026-02-22 18:32:26 +0530

    fix:  add example for problem 743_network_delay_time.py

--- easy/Graph/743_network_delay_time.py
+++ easy/Graph/743_network_delay_time.py
@@ -32,3 +32,13 @@ def networkDelayTime(times, n, k):
         return -1
 
 
+times = [
+    [2, 1, 1],
+    [2, 3, 1],
+    [3, 4, 1]
+]
+
+n = 4
+k = 2
+
+print("Network Delay Time:", networkDelayTime(times, n, k))
